d80e572258d36321ba1f15b183372a8032f8097e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-23 14:47:16 +0100

    made unstyled version of table

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,24 @@ const contacts = [
   ["Peter", "Shaw", ["peter.shaw@mail.com", "peter.shaw@mail.de"]],
   ["Bob", "Andrews", ["bob.andrews@mail.com", "bob.andrews@mail.de"]]
 ];
+var finalText = "<table class=table table-striped table-bordered>";
+finalText =
+  finalText +
+  "<tr><th>First name</th><th>Last name</th><th>Email addresses</th></tr>";
+var append = contacts.map(row => {
+  i = "<tr>";
+  row.map(cell => {
+    if (typeof cell === "object") {
+      myString = cell.join("<br>");
+    } else {
+      myString = cell;
+    }
+    i = i + `<td>${myString}</td>`;
+    return i;
+  });
+  i = i + "</tr>";
+  return i;
+});
+finalText = finalText + append.join("");
+finalText = finalText + "</table>";
+document.getElementById("table").innerHTML = finalText;
