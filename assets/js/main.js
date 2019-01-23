87ebab332d8fbab31c8be02213069449920b2c53
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-23 13:33:55 +0100

    inserted predefined js

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const contacts = [
+  ["Justus", "Jonas", ["justus@mail.com"]],
+  ["Peter", "Shaw", ["peter.shaw@mail.com", "peter.shaw@mail.de"]],
+  ["Bob", "Andrews", ["bob.andrews@mail.com", "bob.andrews@mail.de"]]
+];
